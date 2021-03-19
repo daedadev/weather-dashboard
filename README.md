dc96fe27dbfd14307d996a8b742ff7f53e66bdd8
Daniel Moore <wron2@hotmail.com>
2021-03-18 23:46:29 -0700

    Adding everything to completion

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # weather-dashboard
+
+## Description
+
+The purpose of this project was to create a webpage that pulls from a weather api to display the current weather as well as the current five day forecast for the coming week. The searched cities you enter will be saved locally and displayed underneath the search bar for quick access. This project was a good challenge and I enjoyed the process a lot. Thank you for checking it out and I hope you enjoy.
+
+Link to the finished webpage hosted on Git: https://wron1.github.io/code-refactor/
+
+
+## Usage
+
+General layout of the page
+![Screenshot 1](assets/img/screenshot1.PNG?raw=true)
+
+
+## License
+
+MIT License
+
+Copyright (c) [2021] [Daniel Moore]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,308 @@
+searchBtn = document.getElementById("search-button");
+searchInput = document.getElementById("search-input");
+
+// Current day information
+currentDayDate = document.getElementById("current-day-date");
+currentDayTemp = document.getElementById("current-temperature");
+currentDayHumid = document.getElementById("current-humidity");
+currentDayWind = document.getElementById("current-wind-speed");
+currentDayUV = document.getElementById("current-uv");
+
+futureForecastAdd = document.getElementById("future-forecasts-add");
+
+storedSearches = document.getElementById("stored-searches");
+
+storedButtons = document.getElementsByClassName("stored-buttons");
+
+// Instantiating moment.js
+currentDate = "("+moment().format('l')+")"; 
+
+// Fetching the information on the city typed in the input box
+function getDayResults(theCity){
+
+    if(!theCity){
+
+        return;
+
+    }
+
+    currentCityURL = ("http://api.openweathermap.org/data/2.5/weather?q="+theCity+"&appid=235fab1f9291137bef67ef70be92615e")
+
+    fetch(currentCityURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+
+        var kelvinToFair = data.main.temp * 9/5 - 459.67;
+        kelvinToFair = kelvinToFair.toFixed(2);
+
+        currentDayDate.innerHTML = (data.name + " " + currentDate);
+        currentDayTemp.innerHTML = "Temperature: "+kelvinToFair;
+        currentDayHumid.innerHTML = "Humidity: "+data.main.humidity+"%";
+        currentDayWind.innerHTML = "Wind Speed: "+data.wind.speed+" MPH";
+
+        uvIndexURL = ("http://api.openweathermap.org/data/2.5/uvi?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid=235fab1f9291137bef67ef70be92615e")
+
+        console.log(uvIndexURL);
+
+        fetch(uvIndexURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            console.log(data);
+
+            currentDayUV.innerHTML = data.value;
+
+            setUVStyling(data.value);
+        });
+
+        console.log(data);
+
+    });
+};
+
+// Styling the UV according to the UV value
+function setUVStyling(uvIndex){
+
+    console.log("its working")
+
+    if(uvIndex > 0 && (uvIndex < 3)) {
+
+        currentDayUV.className = "low";
+        console.log("wtf")
+
+    }
+
+    if(uvIndex > 3 && (uvIndex < 6)){
+
+        currentDayUV.className = "medium";
+        console.log("wtf")
+
+    }
+
+    if(uvIndex > 6 && (uvIndex < 8)){
+
+        currentDayUV.className = "high";
+        console.log("wtf")
+
+    }
+
+    if(uvIndex > 8 && (uvIndex < 11)){
+
+        currentDayUV.className = "very-high";
+        console.log("wtf")
+
+    }
+    if(uvIndex > 11 && (uvIndex < 3)){
+
+        currentDayUV.className = "extreme";
+        console.log("wtf")
+
+    }
+}
+
+// Handling all of the future forecast generation
+function futureForcasts(theCity){
+
+    currentCityURL = ("http://api.openweathermap.org/data/2.5/forecast?q="+theCity+"&appid=235fab1f9291137bef67ef70be92615e")
+
+    fetch(currentCityURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.list);
+
+        futureForecastAdd.innerHTML = "";
+
+        // Iterating on the forecasts to create the elements
+        for(i=0; i<5; i++){
+
+            // Setting the variables to go inside 
+            var kelvinToFair = data.list[i].main.temp * 9/5 - 459.67;
+            kelvinToFair = kelvinToFair.toFixed(2);
+
+            var dayArticle = document.createElement('article');
+            dayArticle.setAttribute("class", "days-forecasts")
+
+            var dayDate = document.createElement('h3');
+
+            var weatherIcon = document.createElement('i');
+            weatherIcon.setAttribute("id", "currentImage"+i);
+
+            var dayTemp = document.createElement('h1');
+            var dayHumid = document.createElement('h1');
+
+            // Adding the variables
+            dayDate.innerHTML = moment().add((1+i), 'days').format('L');
+            dayTemp.innerHTML = "Temperature: "+kelvinToFair;
+            dayHumid.innerHTML = "Humidity: "+data.list[i].main.humidity + "%";
+
+            // Putting everything together
+            dayArticle.appendChild(dayDate);
+            dayArticle.appendChild(weatherIcon);
+            dayArticle.appendChild(dayTemp);
+            dayArticle.appendChild(dayHumid);
+            futureForecastAdd.appendChild(dayArticle);
+            
+            // Changing the icon to match the forecast description
+            if(data.list[i].weather.description = "clear sky"){
+
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-sun");
+
+            }
+        
+            if(data.list[i].weather.description = "few clouds"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-cloud-sun");
+                
+            }
+        
+            if(data.list[i].weather.description = "scattered clouds"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-cloud-sun");
+                
+            }
+        
+            if(data.list[i].weather.description = "broken clouds"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-cloud-sun");
+                
+            }
+        
+            if(data.list[i].weather.description = "shower rain"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-cloud-rain");
+                
+            }
+
+            if(data.list[i].weather.description = "rain"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-cloud-showers-heavy");
+                
+            }
+
+            if(data.list[i].weather.description = "thunderstorm"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-poo-storm");
+                
+            }
+
+            if(data.list[i].weather.description = "snow"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "fas fa-snowflake");
+                
+            }
+
+            if(data.list[i].weather.description = "mist"){
+        
+                var theIcon = document.getElementById("currentImage"+i)
+                theIcon.setAttribute("class", "	fas fa-cloud-sun-rain");
+                
+            }
+        }
+    });
+}
+
+function theStoredButtons(cityToStore){
+
+    var storedCities = localStorage.getItem("list of cities");
+
+    if(storedCities){
+
+        storedCities = JSON.parse(storedCities);
+        storedCities.push(cityToStore);
+        localStorage.setItem("list of cities", JSON.stringify(storedCities));
+
+    }else{
+
+        storedCities = []
+        storedCities.push(cityToStore);
+        localStorage.setItem("list of cities", JSON.stringify(storedCities));
+
+    }
+}
+
+function printSaved(){
+
+    var storedCities = localStorage.getItem("list of cities");
+
+    if(storedCities){
+
+        storedCities = JSON.parse(storedCities);
+
+        for(i=0; i<storedCities.length; i++){
+
+            var newButton = document.createElement('button');
+            newButton.innerHTML = storedCities[i];
+            newButton.setAttribute("class", "stored-buttons");
+
+            newButton.addEventListener("click", function(e){
+
+                console.log(this);
+                searchedCity = this.innerHTML;
+                console.log(currentDate);
+                getDayResults(searchedCity);
+                futureForcasts(searchedCity);
+                theStoredButtons(searchedCity);
+
+            })
+
+            storedSearches.appendChild(newButton);
+
+        }
+
+    }
+}
+
+function updateSavedList(){
+
+    var storedCities = localStorage.getItem("list of cities");
+
+    if(storedCities){
+
+        storedCities = JSON.parse(storedCities);
+
+        var newButton = document.createElement('button');
+        newButton.innerHTML = searchInput.value;
+        newButton.setAttribute("class", "stored-buttons");
+
+        newButton.addEventListener("click", function(e){
+
+            console.log(this);
+            searchedCity = this.innerHTML;
+            console.log(currentDate);
+            getDayResults(searchedCity);
+            futureForcasts(searchedCity);
+            theStoredButtons(searchedCity);
+
+        })
+
+        storedSearches.appendChild(newButton);
+
+    }
+}
+
+searchBtn.addEventListener("click", function(){
+
+    searchedCity = searchInput.value;
+    console.log(currentDate);
+    getDayResults(searchedCity);
+    futureForcasts(searchedCity);
+    theStoredButtons(searchedCity);
+    updateSavedList();
+
+});
+
+printSaved();
